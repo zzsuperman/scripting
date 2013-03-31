e2a7c012d1d903a890e538881412eafa0f29a4dd
Chet Witucki <chet.witucki@gmail.com>
2013-03-30 20:09:31 -0400

    cronjob location update

--- cronfg.sh
+++ cronfg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/touch /home/ubuntu/CHET/cronfg.time
-/usr/bin/perl /home/ubuntu/CHET/fieldgrabber.pl -d /home/ubuntu/CHET/TEST
+/usr/bin/touch /home/ubuntu/scripting/cronfg.time
+/usr/bin/perl /home/ubuntu/scripting/fieldgrabber.pl -d /home/ubuntu/scripting/TEST-DEF
